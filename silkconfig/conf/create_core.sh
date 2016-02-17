f351bc5d98bab1479282adcb075e21175f52eaa8
Sebastián Estévez <estevezsebastian@gmail.com>
2016-02-17 09:16:52 -0500

    fix create_core file endingsed
    thanks Patrick

--- silkconfig/conf/create_core.sh
+++ silkconfig/conf/create_core.sh
@@ -5,8 +5,8 @@
 #dsetool create_core silkconfig.silkconfig schema=./schema.xml solrconfig=./solrconfig.xml
 curl http://localhost:8983/solr/resource/silkconfig.silkconfig/solrconfig.xml  --data-binary @solrconfig.xml -H 'Content-type:text/xml; charset=utf-8'
 curl http://localhost:8983/solr/resource/silkconfig.silkconfig/schema.xml  --data-binary @schema.xml -H 'Content-type:text/xml; charset=utf-8'
-curl http://localhost:8983/solr/resource/silkconfig.silkconfig/stopwords_en.xml --data-binary @lang/stopwords_en.xml -H 'Content-type:text/xml; charset=utf-8'
-curl http://localhost:8983/solr/resource/silkconfig.silkconfig/protowords.xml  --data-binary @protowords.xml -H 'Content-type:text/xml; charset=utf-8'
-curl http://localhost:8983/solr/resource/silkconfig.silkconfig/synonyms.xml  --data-binary @synonyms.xml -H 'Content-type:text/xml; charset=utf-8'
-curl http://localhost:8983/solr/resource/silkconfig.silkconfig/stopwords.xml --data-binary @stopwords.xml -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/silkconfig.silkconfig/stopwords_en.txt --data-binary @lang/stopwords_en.txt -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/silkconfig.silkconfig/protowords.txt  --data-binary @protowords.txt -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/silkconfig.silkconfig/synonyms.txt  --data-binary @synonyms.txt -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/silkconfig.silkconfig/stopwords.txt --data-binary @stopwords.txt -H 'Content-type:text/xml; charset=utf-8'
 curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=silkconfig.silkconfig&reindex=true"
